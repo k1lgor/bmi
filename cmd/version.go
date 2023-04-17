26ebb922fa1526c94ff0c5f273c5f6e8dea8e5ac
Plamen Ivanov <paco.iwanow@gmail.com>
2023-04-17 23:30:29 +0300

    chore: change version
    Bump version to 0.1.3 in versionCmd in cmd/version.go.

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ import (
 
 // versionCmd represents the version command
 var (
-	version    = "0.1.0"
+	version    = "0.1.2"
 	versionCmd = &cobra.Command{
 		Use:   "version",
 		Short: "Prints the version number of bmi",
